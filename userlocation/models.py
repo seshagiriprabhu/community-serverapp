2e00960b170f0690e360f489e54c2f0b67cb7d65
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2015-01-07 11:31:25 +0100

    [Fixed]: Model clash - Removed Geofence model from userlocation

--- userlocation/models.py
+++ userlocation/models.py
@@ -1,38 +1,6 @@
 from django.db import models
-from datetime import datetime
-
 from registeration.models import Registeration
-
-class Geofence(models.Model):
-    gid = models.AutoField(primary_key=True)
-    fence_name = models.CharField(max_length=32, blank=False)
-    latitude = models.DecimalField(
-            max_digits=9, decimal_places=2, blank=False)
-    longitude = models.DecimalField(
-            max_digits=9, decimal_places=2, blank=False)
-    geofence_radius = models.DecimalField(
-            max_digits=9, decimal_places=2, blank=False)
-    expiration_time = models.IntegerField(blank=True)
-    email = models.ForeignKey(Registeration)
-    created_date_time = models.DateTimeField(auto_now_add=True,
-            blank=True)
-    
-    def create(self, validated_data):
-        return Geofence.objects.create(**validated_data)
-
-    def save(self, *args, **kwargs):
-        # Expiration time of geofence is set as never expire
-        self.expiration_time = -1 
-        super(Geofence, self).save(*args, **kwargs)
-
-    class Meta:
-        db_table = 'geofence'
-        ordering = ('fence_name', 'latitude', 'longitude',\
-                'geofence_radius', 'email')
-
-    def __unicode__(self):
-        return u'%s, %s' % (self.gid, self.fence_name)
-
+from geofence.models import Geofence
 
 class UserLocationData(models.Model):
     uid = models.AutoField(primary_key=True)
